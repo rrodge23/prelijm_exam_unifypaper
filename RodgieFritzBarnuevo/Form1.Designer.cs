bc45d0cd23421297e2ea2dfed9dbdfcba689b89a
alfredrubrico7 <alfred.rubrico@jmc.edu.ph>
2017-08-05 16:56:24 +0800

    UI for Unify Paper Login Page v1.0.0.1

--- RodgieFritzBarnuevo/Form1.Designer.cs
+++ RodgieFritzBarnuevo/Form1.Designer.cs
@@ -36,8 +36,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -99,7 +99,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(726, 437);
+            this.button1.Location = new System.Drawing.Point(726, 436);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 57);
             this.button1.TabIndex = 7;
@@ -125,17 +125,6 @@
             this.panel1.TabIndex = 8;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(347, 171);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(695, 111);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "UNIFY PAPER";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
@@ -147,6 +136,17 @@
             this.label4.TabIndex = 9;
             this.label4.Text = "INVENTORY MANAGEMENT SYSTEM";
             this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(347, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(695, 111);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "UNIFY PAPER";
             // 
             // frmMain
             // 
